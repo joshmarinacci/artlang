5543d4edf06da1c51993f4114665d603afcd453c
josh <joshua@marinacci.org>
2021-10-10 16:22:00 -0700

    add extra deps for pygamer for the screen

--- templates/pygamer_template.py
+++ templates/pygamer_template.py
@@ -6,16 +6,41 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_debouncer import Debouncer
 from digitalio import DigitalInOut, Pull
+import analogio
 from adafruit_hid.mouse import Mouse
 import usb_hid
+import keypad
 from tasks import TaskMaster
 from common import System
 from common import WHITE, BLACK, RED, GREEN, BLUE
 from pygamer import Canvas
 
 
-print(board)
-# pygamer already has
+# pygamer already has variables for joystick_x, joystick_y, and buttons
+joystick_x = analogio.AnalogIn(board.JOYSTICK_X)
+joystick_y = analogio.AnalogIn(board.JOYSTICK_Y)
+
+buttons = keypad.ShiftRegisterKeys(
+    clock = board.BUTTON_CLOCK,
+    data  = board.BUTTON_OUT,
+    latch = board.BUTTON_LATCH,
+    key_count = 4,
+    value_when_pressed = True,
+    )
+
+# get width with board.DISPLAY.width
+# get height with board.DISPLAY.height
+print("width is",board.DISPLAY.width)
+print("width is",board.DISPLAY.height)
+
+# get joystick_x
+print(joystick_x.value)
+print(joystick_y.value)
+
+# get next event by looking at pad.events.get()
+#    event = pad.events.get()
+#    if event:
+#        print("event happened", event.key_number, event.pressed, event.released)
 
 ${BOARD_IMPORTS}
 
@@ -30,24 +55,11 @@ ${USER_VARIABLES}
 ${USER_FUNCTIONS}
 
 board.DISPLAY.show(g)
-# board.DISPLAY.auto_refresh = True
-
-# up_key = repeat.KeyRepeat(lambda: joystick.up, rate=0.2)
-# down_key = repeat.KeyRepeat(lambda: joystick.down, rate=0.2)
-# left_key = repeat.KeyRepeat(lambda: joystick.left, rate=0.2)
-# right_key = repeat.KeyRepeat(lambda: joystick.right, rate=0.2)
-# buttons = gamepadshift.GamePadShift(digitalio.DigitalInOut(board.BUTTON_CLOCK),
-
-# BUTTON_SEL = const(8)
-# BUTTON_START = const(4)
-# BUTTON_A = const(2)
-# BUTTON_B = const(1)
-
-#joystick = analogjoy.AnalogJoystick()
 
 tm.start()
 while True:
     system.update()
+    board.DISPLAY.show(g)
     tm.cycle(0.01)
 
 print("pygamer end everything")
